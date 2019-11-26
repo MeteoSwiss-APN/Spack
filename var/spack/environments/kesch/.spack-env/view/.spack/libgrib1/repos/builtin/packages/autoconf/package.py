41212ebcb5310678e8d44d16da8c94966d722568
Elsa Germann <egermann@keschln-0003.cm.cluster>
2019-11-26 12:24:39 +0100

    ADD: cosmo, libgrib1 packages

---
+++ var/spack/environments/kesch/.spack-env/view/.spack/libgrib1/repos/builtin/packages/autoconf/package.py
@@ -0,0 +1,1 @@
+/scratch-shared/meteoswiss/scratch/egermann/cosmo-org/elsaspack/opt/spack/linux-rhel7-haswell/gcc-4.8.5/libgrib1-2019-11-22-75wou44prkmuwyzzpuev4kj7rbf564q7/.spack/repos/builtin/packages/autoconf/package.py
---
+++ var/spack/environments/kesch/.spack-env/view/share/locale/pt_BR/LC_MESSAGES/libiconv.mo
@@ -0,0 +1,1 @@
+/scratch-shared/meteoswiss/scratch/egermann/cosmo-org/elsaspack/opt/spack/linux-rhel7-haswell/gcc-4.8.5/libiconv-1.16-xcmzb6amddii7iat3lrfs3r6lnvevaf6/share/locale/pt_BR/LC_MESSAGES/libiconv.mo
