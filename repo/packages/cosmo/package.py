4cd1c8d57154513b57a119ff5b099486caff21a6
Elsa Germann <egermann@tsa-ln002.cm.cluster>
2020-01-24 12:20:18 +0100

    ADD: config.yaml and modules.yaml files

--- repo/packages/cosmo/package.py
+++ repo/packages/cosmo/package.py
@@ -14,7 +14,7 @@ class Cosmo(MakefilePackage):
     url      = "cosmo"
 
     git      = 'git@github.com:COSMO-ORG/cosmo.git'
-    maintainers = ['havogt', 'clementval']
+    maintainers = ['elsagermann']
 
     version('master', branch='master')  
     version('5.05a', commit='ef85dacc25cbadec42b0a7b77633c4cfe2aa9fb9')
@@ -26,17 +26,19 @@ class Cosmo(MakefilePackage):
     depends_on('cuda')
     depends_on('cosmo-dycore%gcc +gpu', when='+gpu')
     depends_on('cosmo-dycore%gcc +test', when='+dycoretest')
-   # depends_on('cosmo-dycore%gcc', when='+cppdycore')
+    depends_on('cosmo-dycore%gcc', when='+cppdycore')
+    depends_on('serialbox@2.6.0', when='+serialize')
+    depends_on('openmpi', when='+serialize')
     depends_on('libgrib1%pgi@19.9-gcc')
     depends_on('cosmo-grib-api')
-    depends_on('jasper@1.900.1')
-    depends_on('perl@5.16.3')
+    depends_on('jasper@1.900.1:')
+    depends_on('perl@5.16')
     depends_on('claw', when='+claw')
 
    # variant('cppdycore', default=True, description='Build with the C++ DyCore')
     variant('dycoretest', default=False, description='Compile Dycore unittest')
     variant('gpu', default=True, description='Build the GPU version of COSMO')
-    variant('serialization', default=False, description='Build with serialization enabled')
+    variant('serialize', default=False, description='Build with serialization enabled')
     variant('parallel', default=True, description='Build parallel COSMO') #TODO enable
     variant('debug', default=False, description='Build debug mode')
     variant('single-precision', default=False, description='Build with single precision enabled')
@@ -58,6 +60,9 @@ class Cosmo(MakefilePackage):
             build.append('CPP_GT_DYCORE=1')
         if '+claw' in self.spec:
             build.append('CLAW=1')
+        if '+serialize' in self.spec:
+            build.append('SERIALIZE=1')
+            build.append('SER_READ_PERT=1')
         target = ''
         if '+parallel' in self.spec:
             target += 'par'
@@ -76,6 +81,8 @@ class Cosmo(MakefilePackage):
           env['BOOST_ROOT'] = spec['boost'].prefix
           env['GRIDTOOLS_DIR'] = spec['gridtools'].prefix
           env['DYCOREGT_DIR'] = spec['cosmo-dycore'].prefix
+        if '+serialize' in self.spec:
+          env['SERIALBOX_DIR'] = spec['serialbox'].prefix
         # sets CLAW paths if variant +claw
         if '+claw' in self.spec:
             env['CLAWDIR'] = '{0}'.format(spec['claw'].prefix) 
@@ -86,7 +93,7 @@ class Cosmo(MakefilePackage):
             makefile.filter('INSTALL_PREFIX=', 'INSTALL_PREFIX={0}'.format(prefix))
             OptionsFileName='Options'
             if self.spec.architecture.target == 'skylake_avx512':
-                OptionsFileName += '.arolla'
+                OptionsFileName += '.tsa'
             if self.compiler.name == 'gcc':
                 OptionsFileName += '.gcc'
             elif self.compiler.name == 'pgi':
@@ -100,7 +107,7 @@ class Cosmo(MakefilePackage):
                 OptionsFileName += '.cpu'
                 optionsfilter = FileFilter('Options.lib.cpu')
             makefile.filter('/Options', '/' + OptionsFileName)
-    
+            
             optionsfilter.filter('GRIBAPII =.*',  'GRIBAPII = -I{0}/include'.format(spec['cosmo-grib-api'].prefix))
             optionsfilter.filter('GRIBAPIL =.*',  'GRIBAPIL = -L{0}/lib -lgrib_api_f90 -lgrib_api -L{1}/libjasper/lib -ljasper'.format(spec['cosmo-grib-api'].prefix, spec['jasper'].prefix))
             optionsfilter.filter('GRIBDWDL =.*',  'GRIBDWDL = -L{0} -lgrib1_{1}'.format(spec['libgrib1'].prefix, self.compiler.name))
@@ -109,17 +116,18 @@ class Cosmo(MakefilePackage):
 
             if '+gpu' in self.spec:
                 optionsfilter.filter('GRIDTOOLSL =.*',  'GRIDTOOLSL = -L{0}/lib -lgcl'.format(spec['gridtools'].prefix))
-                optionsfilter.filter('GRIDTOOLSI =.*',  'GRIDTOOLSI = -I{0}/include/gridtools'.format(spec['gridtools'].prefix))  
-                optionsfilter.filter('DYCOREGTL =.*',  'DYCOREGTL = -L{0}/lib {1} -ldycore -ldycore_base -ldycore_backend -lstdc++ -lcpp_bindgen_generator -lcpp_bindgen_handle -lgt_gcl_bindings'.format(spec['cosmo-dycore'].prefix, '-ldycore_bindings_double -ldycore_base_bindings_double'))
+                optionsfilter.filter('GRIDTOOLSI =.*',  'GRIDTOOLSI = -I{0}/include/gridtools'.format(spec['gridtools'].prefix))
+                if '+single-precision' in self.spec:
+                    optionsfilter.filter('DYCOREGTL =.*',  'DYCOREGTL = -L{0}/lib {1} -ldycore -ldycore_base -ldycore_backend -lstdc++ -lcpp_bindgen_generator -lcpp_bindgen_handle -lgt_gcl_bindings'.format(spec['cosmo-dycore'].prefix, '-ldycore_bindings_float -ldycore_base_bindings_float'))
+                else:
+                    optionsfilter.filter('DYCOREGTL =.*',  'DYCOREGTL = -L{0}/lib {1} -ldycore -ldycore_base -ldycore_backend -lstdc++ -lcpp_bindgen_generator -lcpp_bindgen_handle -lgt_gcl_bindings'.format(spec['cosmo-dycore'].prefix, '-ldycore_bindings_double -ldycore_base_bindings_double'))
                 optionsfilter.filter('DYCOREGTI =.*',  'DYCOREGTI = -I{0}'.format(spec['cosmo-dycore'].prefix))
 
-            #TODO if variant serialization:
-            """
-            optionsfilter.filter('SERIALBOX =.*',  'SERIALBOX = '.format(spec['serialbox'].prefix))
-      
-            SERIALBOXL = ${SERIALBOX_FORTRAN_LIBRARIES} -lstdc++
-            SERIALBOXI = -I$(SERIALBOX)/include
-            """
+            if '+serialize' in self.spec:
+                optionsfilter.filter('MPII     =.*',  'MPII     = -I{0}/include'.format(spec['openmpi'].prefix))
+                optionsfilter.filter('MPIL     =.*',  'MPIL     = -L{0}/lib -lmpi -lmpi_cxx'.format(spec['openmpi'].prefix))
+                optionsfilter.filter('SERIALBOXI =.*',  'SERIALBOXI = -I{0}/include/'.format(spec['serialbox'].prefix))
+                optionsfilter.filter('SERIALBOXL =.*',  'SERIALBOXL = {0}/lib/libSerialboxFortran.a {0}/lib/libSerialboxC.a -lstdc++fs -lpthread {0}/lib/libSerialboxCore.a -lstdc++'.format(spec['serialbox'].prefix))
 
     def install(self, spec, prefix):
         with working_dir(self.build_directory):
